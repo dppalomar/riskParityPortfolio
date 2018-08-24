35598c3e347403a3dd8c9953849007ce6ec8ce16
mirca <jvmirca@gmail.com>
2018-08-24 06:16:03 -0300

    vectorize d1 and d2 functions

--- R/sucessiveConvexApprox.R
+++ R/sucessiveConvexApprox.R
@@ -2,7 +2,7 @@
 # problem described in FengPalomar-ICASSP2016.pdf
 
 theta_update <- function(theta, x, w, g, gamma) {
-  theta_hat <- sum(x * g(w))
+  theta_hat <- sum(x * g(w, Sigma))
   return(theta + gamma * (theta_hat - theta))
 }
 
@@ -12,14 +12,23 @@ w_update <- function(...) {
 
 
 d1 <- function(x, p = 0.05, e = 1e-4) {
-  if (abs(x) < e) {
-    return abs(x) / (e * (p + e) * log(1 + 1 / p))
-  } else {
-    return 1 / ((abs(x) + p) * log(1 + 1 / p))
-  }
+  d1x <- rep(0, length(x))
+  mask <- (abs(x) <= e)
+  d1x[mask] <- abs(x) / (e * (p + e))
+  d1x[!mask] <- 1 / ((abs(x) + p))
+  d1x <- d1x / log(1 + 1 / p)
+  return(d1x)
 }
 
 
 d2 <- function(x, p = 0.05, e = 1e-4) {
-  return(NA)
+  d2x <- rep(0, length(x))
+  mask <- (abs(x) <= e)
+  d2x[mask] <- 1 / (e * (p + e))
+  d2x[!mask] <- 1 / (abs(x) * (abs(x) + p))
+  d2x <- d2x / (2 * log(1 + 1 / p))
+  return(d2x)
+}
+
+g <- function(w, Sigma) {
 }
