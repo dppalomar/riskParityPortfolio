28ff2a4e396817112d7f9d3957704a613d33ef97
mirca <jvmirca@gmail.com>
2018-08-29 09:53:59 -0300

    save loglikelihood and objective function values

--- R/riskParityPortfolio.R
+++ R/riskParityPortfolio.R
@@ -16,6 +16,9 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
     constraints <- list(constraints, w >= 0)
   }
 
+  nll_seq <- c()
+  fun_seq <- c()
+
   w <- CVXR::Variable(length(mu))
   for k in (1:maxiter) {
     # auxiliary quantities
@@ -23,12 +26,18 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
     gk <- t(g(wk, Sigma))
     Qk <- 2 * AkT %*% Ak + tau * I
     qk <- 2 * AkT %*% g(wk) - Qk %*% wk
-    # build problem (39) as in Feng & Palomar TSP2015
+    # build and solve problem (39) as in Feng & Palomar TSP2015
     F <- negLogLikelihoodCVX(w, nu, mu, Sigma)
     P <- .5 * CVXR::quad_form(w, Qk) + t(w) %*% qk
-    w_hat <- solve(CVXR::Problem(CVXR::Minimize(P + lambda * F),
-                                 constraints = constraints))$getValue(w)
+    obj_fun <- CVXR::Minimize(P + lambda * F)
+    prob <- CVXR::Problem(obj_fun, constraints = constraints)
+    result <- solve(prob)
+    w_hat <- result$getValue(w)
     w_next <- wk + gamma * (w_hat - wk)
+    # save likelihood and objective function values
+    nll_seq <- c(nll_seq, negLoglikelihood(wk, nu, mu, Sigma))
+    funk <- result$value
+    fun_seq <- c(fun_seq, funk)
     # update variables
     wk <- w_next
     gamma <- gamma * (1 - zeta * gamma)
