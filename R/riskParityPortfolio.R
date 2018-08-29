e19e1429d05e803fd9e3a4893f1311cccd60b848
mirca <jvmirca@gmail.com>
2018-08-29 18:44:11 -0300

    use default tau from the paper

--- R/riskParityPortfolio.R
+++ R/riskParityPortfolio.R
@@ -3,13 +3,17 @@
 # as presented in FengPalomar-TSP2015.pdf
 # @'export
 riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
-                                   w0 = NA, gamma = .9, zeta = .1, tau = .5,
-                                   lambda = .5, maxiter = 500) {
+                                   w0 = NA, gamma = .9, zeta = 1e-7, tau = NA,
+                                   lambda = .5, maxiter = 500, ftol = 1e-5,
+                                   wtol = 1e-5) {
   if (any(is.na(w0))) {
     wk <- 1 / diag(Sigma)
     wk <- wk / sum(wk)
   } else {
     wk <- w0
+  }
+  if (is.na(tau)) {
+    tau <- .05 * sum(diag(Sigma)) / (2 * length(mu))
   }
   constraints <- list(sum(w) == 1)
   if (!shortselling) {
@@ -40,8 +44,8 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
     fun_next <- result$value
     fun_seq <- c(fun_seq, fun_next)
     # check convergence on parameters
-    w_err <- norm(w_next - wk, "2") / max(1., norm(w_next, "2"))
-    if (w_err < wtol) {
+    werr <- norm(w_next - wk, "2") / max(1., norm(w_next, "2"))
+    if (werr < wtol) {
       wk <- w_next
       break
     }
