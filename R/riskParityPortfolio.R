cbcef17d7f4835ac80533bc607a966f151b4bac9
mirca <jvmirca@gmail.com>
2018-08-30 04:40:37 -0300

    be more explicit in the code

--- R/riskParityPortfolio.R
+++ R/riskParityPortfolio.R
@@ -7,16 +7,17 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
                                    w0 = NA, gamma = .9, zeta = 1e-7, tau = NA,
                                    lambda = .5, maxiter = 500, ftol = 1e-5,
                                    wtol = 1e-5) {
+  N <- nrow(Sigma)
   if (any(is.na(w0))) {
-    wk <- rep(1 / nrow(Sigma), nrow(Sigma))
+    wk <- rep(1 / N, N)
   } else {
     wk <- w0
   }
   if (is.na(tau)) {
-    tau <- .05 * sum(diag(Sigma)) / (2 * nrow(Sigma))
+    tau <- .05 * sum(diag(Sigma)) / (2 * N)
   }
   # build constraints
-  w <- CVXR::Variable(nrow(Sigma))
+  w <- CVXR::Variable(N)
   constraints <- list(sum(w) == 1)
   if (!shortselling) {
     constraints <- c(constraints, w >= 0)
@@ -32,7 +33,7 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
     Qk <- 2 * AkT %*% t(AkT) + tau * diag(nrow(AkT))
     qk <- 2 * AkT %*% g(wk, Sigma) - Qk %*% wk
     # build and solve problem (39) as in Feng & Palomar TSP2015
-    F <- negLogLikelihoodCVX(w, nu, mu, Sigma)
+    F <- CVXR::quad_form(w, Sigma) - nu * t(w) %*% mu
     P <- .5 * CVXR::quad_form(w, Qk) + t(w) %*% qk
     obj_fun <- CVXR::Minimize(P + lambda * F)
     prob <- CVXR::Problem(obj_fun, constraints = constraints)
@@ -40,7 +41,7 @@ riskParityPortfolioCVX <- function(mu, Sigma, nu = 0, shortselling = FALSE,
     w_hat <- result$getValue(w)
     w_next <- wk + gamma * (w_hat - wk)
     # save likelihood and objective function values
-    nll_seq <- c(nll_seq, negLogLikelihood(w_next, nu, mu, Sigma))
+    nll_seq <- c(nll_seq, t(w_next) %*% Sigma %*% w_next - nu * t(w_next) %*% mu)
     fun_next <- result$value
     fun_seq <- c(fun_seq, fun_next)
     # check convergence on parameters
