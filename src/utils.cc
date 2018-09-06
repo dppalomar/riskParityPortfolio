f884d81fc565e6d1a256c5be3db1f657afa47a0c
mirca <jvmirca@gmail.com>
2018-09-05 23:21:42 -0300

    minor speed up

--- src/utils.cc
+++ src/utils.cc
@@ -11,10 +11,13 @@ Eigen::MatrixXd computeACpp(const Eigen::VectorXd& w, const Eigen::MatrixXd& Sig
   for (int i = 0; i < N; ++i) {
     Eigen::MatrixXd Mi = Eigen::MatrixXd::Zero(N, N);
     Mi.row(i) = Sigma.row(i);
-    for (int j = 0; j < N; ++j) {
+    for (int j = i+1; j < N; ++j) {
+      if (i == j)
+        continue;
       Eigen::MatrixXd Mj = Eigen::MatrixXd::Zero(N, N);
       Mj.row(j) = Sigma.row(j);
       A.row(i + j * N) = (Mi + Mi.transpose() - Mj - Mj.transpose()) * w;
+      A.row(j + i * N) = A.row(i + j * N);
     }
   }
 
