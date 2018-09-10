beb1ed142633296492a5b35313197f6587b8ec21
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2018-09-10 17:09:50 +0800

    Minor things

--- R_buildignore/developer_commands.R
+++ R_buildignore/developer_commands.R
@@ -4,24 +4,24 @@
 # Local installation
 install.packages(file.choose(), repos = NULL, type="source")
 # Installation from GitHub
-devtools::install_github("dppalomar/spectralGraphTopology")
+devtools::install_github("dppalomar/riskParityPortfolio")
 # Installation from CRAN
 install.packages("spectralGraphTopology")
 # Getting help
-library(spectralGraphTopology)
-help(package="spectralGraphTopology")
-package?spectralGraphTopology
-?learnGraphTopology
+library(riskParityPortfolio)
+help(package="riskParityPortfolio")
+package?riskParityPortfolio
+?riskParityPortfolioSCA
 
 
 ##
 ## Developer commands (http://r-pkgs.had.co.nz/)
 ##
-library(devtools)
 devtools::load_all()  #or Ctrl-Shift-L
 devtools::install()
+library(riskParityPortfolio)
 #devtools::build()  # to generate the installation file
 
 # Documentation
 devtools::document()  #to generate all documentation via roxygen
-?learnGraphTopology
+?riskParityPortfolioSCA
