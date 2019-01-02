f18c6708346f08b24952d7ec73cfef92c593d015
mirca <jvmirca@gmail.com>
2019-01-02 05:53:53 -0300

    improve tests

--- tests/testthat/test-newton-cyclical.R
+++ tests/testthat/test-newton-cyclical.R
@@ -7,9 +7,9 @@ V <- matrix(rnorm(N^2), N, N)
 Sigma <- V %*% t(V)
 
 test_that("Newton and Cyclical Coordinate Descent give the same results", {
-  newton <- riskParityPortfolioNewton(Sigma)
-  cyclical_roncalli <- riskParityPortfolioCyclicalRoncalli(Sigma)
-  cyclical_spinu <- riskParityPortfolioCyclicalSpinu(Sigma)
+  newton <- riskParityPortfolio(Sigma, method_init = "newton")
+  cyclical_roncalli <- riskParityPortfolio(Sigma, method_init = "cyclical-roncalli")
+  cyclical_spinu <- riskParityPortfolio(Sigma, method_init = "cyclical-spinu")
   expect_that(all(abs(newton$w - cyclical_roncalli$w) < 1e-4), is_true())
   expect_that(all(abs(newton$risk_contribution - cyclical_roncalli$risk_contribution) < 1e-4), is_true())
   expect_that(all(abs(newton$w - cyclical_spinu$w) < 1e-4), is_true())
