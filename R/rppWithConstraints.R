db77c78816f0caa20cec0c8d5751106c077f779b
mirca <jvmirca@gmail.com>
2019-08-12 08:45:19 -0300

    maint: bound maxiter

--- R/rppWithConstraints.R
+++ R/rppWithConstraints.R
@@ -13,16 +13,15 @@ rpp_eq_and_ineq_constraints_iteration_R <- function(Cmat, cvec, Dmat, dvec, Qk,
                                                     qk, wk,
                                                     dual_mu_0, dual_mu_minus_1,
                                                     dual_lmd_0, dual_lmd_minus_1,
-                                                    wtol = 1e-6) {
+                                                    wtol = 1e-6, maxiter = 500) {
   B <- rbind(Cmat, Dmat)
   inv_Qk <- solve(Qk)
   L <- norm(B %*% inv_Qk %*% t(B), type = "2")
   #L <- norm(B %*% solve(Qk, t(B)), type = "2")
-  i <- 0
   w_tilde_i_minus_1 <- wk
   dual_lmd_i <- dual_lmd_0;                dual_mu_i <- dual_mu_0
   dual_lmd_i_minus_1 <- dual_lmd_minus_1;  dual_mu_i_minus_1 <- dual_mu_minus_1
-  repeat {
+  for (i in c(0:maxiter)) {
     w_tilde_i <- -inv_Qk %*% (qk + t(Cmat) %*% dual_lmd_i + t(Dmat) %*% dual_mu_i)
     #w_tilde_i <- -solve(Qk, qk + t(Cmat) %*% dual_lmd_i + t(Dmat) %*% dual_mu_i)
     w_tilde_bar_i <- w_tilde_i + (i-1)/(i+2) * (w_tilde_i - w_tilde_i_minus_1)
@@ -30,7 +29,6 @@ rpp_eq_and_ineq_constraints_iteration_R <- function(Cmat, cvec, Dmat, dvec, Qk,
     dual_mu_i_plus_1  <- pmax(0, dual_mu_i  + (i-1)/(i+2) * (dual_mu_i  - dual_mu_i_minus_1 ) + 1/L*(Dmat %*% w_tilde_bar_i - dvec))
     if (i > 0 && all(abs(w_tilde_bar_i - w_tilde_bar_i_minus_1) <=
                      .5 * wtol * (abs(w_tilde_bar_i) + abs(w_tilde_bar_i_minus_1)))) break;
-    i <- i+1
     w_tilde_i_minus_1 <- w_tilde_i
     w_tilde_bar_i_minus_1 <- w_tilde_bar_i
     dual_lmd_i_minus_1 <- dual_lmd_i;  dual_mu_i_minus_1 <- dual_mu_i
