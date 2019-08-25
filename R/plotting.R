928cf4a6e636b9bda1920ef4a10a53b1e9c8d7b1
Daniel P. Palomar <daniel.p.palomar@gmail.com>
2019-08-25 14:52:02 +0800

    Fixing things for CRAN submission to pass the checks...

--- R/plotting.R
+++ R/plotting.R
@@ -27,7 +27,9 @@
 #' names(w_single) <- LETTERS[1:N]
 #' 
 #' w_multiple <- matrix(runif(4*N), ncol = 4)
-#' w_multiple <- sweep(w_multiple, MARGIN = 2, STATS = colSums(w_multiple), FUN = "/")  # normalize each column
+#' w_multiple <- sweep(w_multiple,  # normalize each column
+#'                     MARGIN = 2, 
+#'                     STATS = colSums(w_multiple), FUN = "/")
 #' rownames(w_multiple) <- LETTERS[1:N]
 #' 
 #' # plot
@@ -39,7 +41,7 @@
 #' @author Daniel P. Palomar and Ze Vinicius
 #' 
 #' @export
-barplotPortfolioRisk <- function(w, Sigma, type = c("ggplot2", "simple", "ggplot2-old"), colors = NULL) {
+barplotPortfolioRisk <- function(w, Sigma, type = c("ggplot2", "simple"), colors = NULL) {
   w <- as.matrix(w)
   if (is.null(colnames(w)))
     colnames(w) <- paste0("portf-", 1:ncol(w))
@@ -52,15 +54,15 @@ barplotPortfolioRisk <- function(w, Sigma, type = c("ggplot2", "simple", "ggplot
   switch(match.arg(type),
          "simple" = {
            if (is.null(colors))
-             colors <- topo.colors(ncol(w))
-           old_par <- par(mfrow=c(2,1))
-           barplot(t(w), col = colors,
-                   beside = ncol(w) > 1, legend = colnames(w),
-                   main = "Portfolio weight allocation", ylab = "capital")
-           barplot(t(RRC), col = colors,
-                   beside = ncol(w) > 1,
-                   main = "Relative risk contribution", xlab = "stocks", ylab = "risk")
-           par(old_par)
+             colors <- grDevices::topo.colors(ncol(w))
+           old_par <- graphics::par(mfrow=c(2,1))
+           graphics::barplot(t(w), col = colors,
+                             beside = ncol(w) > 1, legend = colnames(w),
+                             main = "Portfolio weight allocation", ylab = "capital")
+           graphics::barplot(t(RRC), col = colors,
+                             beside = ncol(w) > 1,
+                             main = "Relative risk contribution", xlab = "stocks", ylab = "risk")
+           graphics::par(old_par)
          },
          "ggplot2" = {
            if (!requireNamespace("ggplot2", quietly = TRUE)) 
@@ -68,8 +70,8 @@ barplotPortfolioRisk <- function(w, Sigma, type = c("ggplot2", "simple", "ggplot
            molten_portf_matrix <- rbind(cbind(melt_portf_matrix(w), type = "w"),
                                         cbind(melt_portf_matrix(RRC), type = "RRC"))
            labels <- c(w = "Weight allocation", RRC = "Relative risk contribution")
-           p <- ggplot2::ggplot(molten_portf_matrix, ggplot2::aes(x = stock, y = value)) + 
-             ggplot2::geom_bar(ggplot2::aes(fill = portfolio), color = "black", stat = "identity", position = "dodge", width = 0.8) +
+           p <- ggplot2::ggplot(molten_portf_matrix, ggplot2::aes_string(x = "stock", y = "value")) + 
+             ggplot2::geom_bar(ggplot2::aes_string(fill = "portfolio"), color = "black", stat = "identity", position = "dodge", width = 0.8) +
              ggplot2::facet_wrap(~ type, ncol = 1, scales = "free", labeller = ggplot2::labeller(type = labels)) +
              ggplot2::labs(title = "Portfolio capital and risk distribution", x = "stocks", y = NULL) + 
              ggplot2::theme(legend.title = ggplot2::element_blank()) +
@@ -80,31 +82,6 @@ barplotPortfolioRisk <- function(w, Sigma, type = c("ggplot2", "simple", "ggplot
              p <- p + ggplot2::theme(legend.position = "none")
            p
          },
-         "ggplot2-old" = {
-           # stock <- portfolio <- value <- NULL  # ugly hack to deal with CRAN note
-           p1 <- ggplot2::ggplot(data = melt_portf_matrix(w), ggplot2::aes(x = stock, y = value)) + 
-             ggplot2::geom_bar(ggplot2::aes(fill = portfolio), color = "black", stat = "identity", position = "dodge", width = 0.8) + 
-             ggplot2::scale_fill_manual(values = colors) +
-             ggplot2::labs(title = "Portfolio weight allocation", x = "stocks", y = "capital") + 
-             ggplot2::theme(legend.title = ggplot2::element_blank())
-           p2 <- ggplot2::ggplot(data = melt_portf_matrix(RRC), ggplot2::aes(x = stock, y = value)) + 
-             ggplot2::geom_bar(ggplot2::aes(fill = portfolio), color = "black", stat = "identity", position = "dodge", width = 0.8) + 
-             ggplot2::scale_fill_manual(values = colors) +
-             ggplot2::labs(title = "Relative risk contribution", x = "stocks", y = "risk") + 
-             ggplot2::theme(legend.title = ggplot2::element_blank())
-           # theme(plot.title = element_text(size = 16),
-           #       axis.title = element_text(size = 14, color = '#555555'),
-           #       axis.text = element_text(size = 11),
-           #       legend.position = c(0.9, 0.86), #c(0.905, 0.88), 
-           #       legend.title = element_blank(), 
-           #       legend.spacing.x = unit(4, 'pt'),
-           #       legend.text = element_text(size = 13))
-           if (ncol(w) == 1) {  # remove legend if only one portfolio
-             p1 <- p1 + ggplot2::theme(legend.position = "none")
-             p2 <- p2 + ggplot2::theme(legend.position = "none")
-           }           
-           gridExtra::grid.arrange(p1, p2, nrow = 2)
-         },         
          stop("Barplot type unknown."))
 }
 
@@ -118,22 +95,21 @@ melt_portf_matrix <- function (data, na.rm = FALSE, as.is = FALSE)
   var.convert <- function(x) {
     if (!is.character(x)) 
       return(x)
-    x <- type.convert(x, as.is = TRUE)
+    x <- utils::type.convert(x, as.is = TRUE)
     if (!is.character(x)) 
       return(x)
     factor(x, levels = unique(x))
   }
   dn <- dimnames(data)
   names(dn) <- varnames
-  if (!as.is) {
+  if (!as.is)
     dn <- lapply(dn, var.convert)
-  }
   labels <- expand.grid(dn, KEEP.OUT.ATTRS = FALSE, stringsAsFactors = FALSE)
   if (na.rm) {
     missing <- is.na(data)
     data <- data[!missing]
     labels <- labels[!missing, ]
   }
-  value_df <- setNames(data.frame(as.vector(data)), value.name)
+  value_df <- stats::setNames(data.frame(as.vector(data)), value.name)
   cbind(labels, value_df)
 }
