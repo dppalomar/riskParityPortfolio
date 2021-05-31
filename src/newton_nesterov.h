1b364be0a29f2a1bb2efba38af0823000016ff29
mirca <jvmirca@gmail.com>
2021-05-31 10:54:15 +0800

    [MAINT]: rm function signature from incorrect header

--- src/newton_nesterov.h
+++ src/newton_nesterov.h
@@ -8,5 +8,4 @@ using namespace Eigen;
 VectorXd risk_parity_portfolio_nn(const MatrixXd&, const VectorXd&, const double, const int);
 VectorXd gradient_log_formulation(const MatrixXd&, const VectorXd&, const VectorXd&);
 MatrixXd hessian_log_formulation(const MatrixXd&, const VectorXd&, const VectorXd&);
-double obj_function_spinu(const MatrixXd&, const VectorXd&, const VectorXd&);
 #endif
